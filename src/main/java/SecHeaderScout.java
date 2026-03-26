e0f46294a5357a9ef3edb51a9c4c57dc952fc6d4
ismailsanan <ismailsanan@protonmail.com>
2026-03-26 18:42:29 +0100

    writing output stream

--- src/main/java/SecHeaderScout.java
+++ src/main/java/SecHeaderScout.java
@@ -1,9 +1,15 @@
 import burp.api.montoya.BurpExtension;
 import burp.api.montoya.MontoyaApi;
+import burp.api.montoya.logging.Logging;
 
 public class SecHeaderScout implements BurpExtension {
     @Override
     public void initialize(MontoyaApi api) {
+    api.extension().setName("SecHeaderScout");
+    Logging logging  = api.logging();
+
+    // write a message to our output stream
+        logging.logToOutput("Hello output.");
 
     }
 }
