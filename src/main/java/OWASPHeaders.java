17c78ae0baa4cf0cec04d59675026663f73af20c
ismailsanan <ismailsanan@protonmail.com>
2026-04-01 18:39:09 +0200

    started implementing dynamic solution for headers

--- src/main/java/OWASPHeaders.java
+++ src/main/java/OWASPHeaders.java
@@ -1,3 +1,6 @@
+import burp.api.montoya.MontoyaApi;
+import burp.api.montoya.http.message.HttpRequestResponse;
+import burp.api.montoya.http.message.requests.HttpRequest;
 import java.util.List;
 
 
@@ -6,6 +9,13 @@ import java.util.List;
 //take matti suggestion call the headers dynamically  https://github.com/OWASP/www-project-secure-headers/blob/master/ci/headers_add.json
 public class OWASPHeaders {
 
+    public final MontoyaApi api;
+
+    public OWASPHeaders(MontoyaApi api) {
+        this.api = api;
+    }
+
+    // static OWASP List
     public static final List<String> REQUIRED_HEADERS = List.of(
             "X-DNS-Prefetch-Control",
             "Cache-Control",
@@ -20,4 +30,21 @@ public class OWASPHeaders {
             "X-Frame-Options",
             "Strict-Transport-Security"
     );
+
+
+
+
+    // implement a dynamic solution for the headers by fetching data from  https://raw.github.com/OWASP/www-project-secure-headers/blob/master/ci/headers_add.json
+    public final List<String> Dynamic_REQUEST_HEADERS() {
+
+        HttpRequest request = HttpRequest.httpRequestFromUrl("https://raw.github.com/OWASP/www-project-secure-headers/blob/master/ci/headers_add.json");
+
+        HttpRequestResponse response = api.http().sendRequest(request);
+
+        //Json Structure
+
+
+        return null;
+
+    };
 }
